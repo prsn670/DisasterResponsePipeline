06d3071f898527bb87e0e9be76def92b27e1a6cb
Anil <parshad.anil@gmail.com>
2021-05-19 19:37:01 -0500

    feature: finish ETL class. Add split category column into merged database. create database from df

--- data/process_data.py
+++ data/process_data.py
@@ -7,5 +7,7 @@ if __name__ == "__main__":
     etl_obj = ETL(root)
     etl_obj.load()
     etl_obj.merge_df()
+    etl_obj.clean_df()
+    etl_obj.df_insert_db()
 
     print(etl_obj.get_merged_df().head())
--- etl/ETL.py
+++ etl/ETL.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import re
+from sqlalchemy import create_engine
 
 class ETL:
 
@@ -8,6 +9,7 @@ class ETL:
         self.df_merged = None
         self.df_messages = None
         self.df_categories = None
+        self.engine = None
 
     def load(self, messages_csv="data/messages.csv", categories_csv="data/categories.csv"):
         """
@@ -25,10 +27,41 @@ class ETL:
         :return:
         """
         self.df_merged = pd.merge(self.df_messages, self.df_categories, on=['id', 'id'])
+        self.split_category()
 
     def get_merged_df(self):
         return self.df_merged
 
-    # TODO Split categories, convert to 0 or 1, and replace column with new category columns
-    # TODO remove duplicates
-    # TODO save dataset into sqlite DB
+    def split_category(self):
+        """
+        Splits the data in the categories column and creates a new columns based on the category name.
+        The split values are then moved back into df_merged
+        :return:
+        """
+        # create new category column names
+        category_col_name = re.split('-\d;?', self.df_categories['categories'][0])
+
+        # split values in category column and create new data frame
+        category_series = pd.Series(self.df_merged['categories'])
+        split_categories_df = category_series.str.split(pat=";", expand=True)
+
+        # rename columns
+        for column, new_name in zip(split_categories_df, category_col_name):
+            split_categories_df.rename(columns={column: new_name}, inplace=True)
+        # convert values to 0 or 1
+        split_categories_df.replace(to_replace='\w+-', value="", inplace=True, regex=True)
+
+        # concatenate split data frame into merged data frame and drop old category column
+        self.df_merged = pd.concat([self.df_merged, split_categories_df], axis=1)
+
+    def clean_df(self):
+        self.df_merged.drop(['categories'], axis=1, inplace=True)
+
+        # remove duplicates
+        self.df_merged.drop_duplicates(subset=['id'], inplace=True)
+
+    def df_insert_db(self):
+        # save dataset into sqlite DB
+        # sqlalchemy.delete
+        self.engine = create_engine('sqlite:///DisasterMessage.db')
+        self.df_merged.to_sql('DisasterMessage', self.engine, index=False, if_exists='replace')
